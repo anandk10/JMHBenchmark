126132f500d453f3c93e1d80bce0b1deffdef750
Anand Karandikar <anandk10@gmail.com>
2024-05-26 18:06:25 -0700

    adding a method for benchmarking

--- src/main/java/MyFirstBenchmark.java
+++ src/main/java/MyFirstBenchmark.java
@@ -1,3 +1,12 @@
 public class MyFirstBenchmark {
+    // Benchmark methods will go here
+
+    public long sumArray(int[] array) {
+        long sum = 0;
+        for (int i : array) {
+            sum += i;
+        }
+        return sum;
+    }
     
 }
