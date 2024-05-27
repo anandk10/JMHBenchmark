7a970c9948ee9d14760bd843375af36571f369a9
Anand Karandikar <anandk10@gmail.com>
2024-05-26 18:29:33 -0700

    create a @Benchmark Method

--- src/test/java/TestMyFirstBenchmark.java
+++ src/test/java/TestMyFirstBenchmark.java
@@ -1,3 +1,12 @@
+import org.openjdk.jmh.annotations.Benchmark;
+
 public class TestMyFirstBenchmark {
-    
+
+    MyFirstBenchmark myFirstBenchmark = new MyFirstBenchmark();
+
+    @Benchmark
+    public void testSumArray() {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        myFirstBenchmark.sumArray(array);
+    }
 }
