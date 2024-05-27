55c496b666477b050f2d2fe47a7f4a520def72c5
Anand Karandikar <anandk10@gmail.com>
2024-05-26 18:32:43 -0700

    preparing an array for benchmark state

--- src/test/java/BenchmarkState.java
+++ src/test/java/BenchmarkState.java
@@ -1,6 +1,16 @@
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 
 @State(Scope.Thread)
 public class BenchmarkState {
+    int[] array;
+
+    @Setup
+    public void prepare() {
+        array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = i;
+        }
+    }
 }
